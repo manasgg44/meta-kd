9afa6360aefe222b3260b0d157edc9d07ddd00a6
manas <manasgg44@gmail.com>
2026-02-23 21:30:44 +0530

    debug

--- run_teacher_test.sh
+++ run_teacher_test.sh
@@ -3,6 +3,7 @@
 python test_teacher.py \
 	--teacher resnet32x4 \
 	--save-folder './save/student_model/S:resnet8_T:resnet32x4_cifar100_mlkd_a:None_1' \
+	--checkpoint "${CHECKPOINT:-}" \
 	--batch-size 64 \
 	--num-workers 8 \
 	--print-freq 100
