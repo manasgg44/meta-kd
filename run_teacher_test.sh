cb4ab8401db686338751a2e95bab409c3ef503be
manas <manasgg44@gmail.com>
2026-02-24 22:12:13 +0530

    debug

--- run_teacher_test.sh
+++ run_teacher_test.sh
@@ -2,8 +2,8 @@
 
 python test_teacher.py \
   --teacher "resnet110" \
-  --save_folder "./save/student_model/S:resnet32_T:resnet110_cifar100_mlkd_a:0.5_1/resnet110_teacher_last.pth" \
+  --save_folder "./save/student_model/S:resnet32_T:resnet110_cifar100_mlkd_a:0.5_1" \
   --checkpoint "" \
-  --batch_size 64 \
+  --batch_size 256 \
   --num_workers 8 \
   --print_freq 100
