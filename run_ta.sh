8c5991c4ead163dd8e0e7d5bf647bbb9ac3b43ca
manas <manasgg44@gmail.com>
2026-02-25 23:17:46 +0530

    debug

--- run_ta.sh
+++ run_ta.sh
@@ -6,6 +6,6 @@ CUDA_VISIBLE_DEVICES=1 python train_student_meta_ta.py \
   --model_t resnet110 \
   --path_t ./save/models/resnet110_vanilla/ckpt_epoch_240.pth \
   --loss_type kl --kd_T 4 \
-  -a 0.5 -b 0.5 \
+  -a 0.5 -b 0.7 \
   --held_size 5000 --num_held_samples 100 --num_meta_batches 1 \
   --assume_s_step_size 0.05
